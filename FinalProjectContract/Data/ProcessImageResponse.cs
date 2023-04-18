edf719a37d482fe213e83ff89730107bcd0e6b03
Ran Sasson <ransas88@gmail.com>
2023-04-18 22:33:09 +0300

    Added docker support and DataModel

--- FinalProjectContract/Data/ProcessImageResponse.cs
+++ FinalProjectContract/Data/ProcessImageResponse.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FinalProjectDataModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace FinalProjectContract.Data
 {
     public class ProcessImageResponse
     {
+        public List<PersonData> People { get; set; }
     }
 }
---
+++ FinalProjectDataModel/PersonData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProjectDataModel
+{
+    public class PersonData
+    {
+        public string Pose { get; set; }
+        public Centroid Centroid { get; set; }
+    }
+}
